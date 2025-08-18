0d0f4d7267235f3ffd67c85f8372e3216e007a95
rvsemenov <rvsemenov@domclick.ru>
2025-08-18 17:16:47 +0300

    add cash module

---
+++ cash/src/test/java/com/semenov/cash/CashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semenov.cash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
